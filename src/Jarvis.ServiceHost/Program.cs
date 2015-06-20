8615b6af910cf9d109e19ba678705a50f424d976
Andrea Balducci <mtb.snowboard@gmail.com>
2015-06-20 16:27:26 +0200

    added log4net and topshelf service

--- src/Jarvis.ServiceHost/Program.cs
+++ src/Jarvis.ServiceHost/Program.cs
@@ -3,13 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jarvis.ServiceHost.Support;
+using Topshelf;
 
 namespace Jarvis.ServiceHost
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (Environment.UserInteractive)
+            {
+                Banner();
+            }
+
+            Bootstrapper.Init();
+            
+            var exitCode = HostFactory.Run(host =>
+            {
+                host.UseOldLog4Net("log4net.config");
+
+                host.Service<Bootstrapper>(service =>
+                {
+                    service.ConstructUsing(() => new Bootstrapper());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                host.RunAsNetworkService();
+
+                host.SetDescription("Jarvis - Sample App");
+                host.SetDisplayName("Jarvis - Sample App");
+                host.SetServiceName("JarvisSampleApp");
+            });
+
+            return (int)exitCode;
+        }
+
+        private static void Banner()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("Jarvis Sample App - Proximo srl");
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("  install                        -> install service");
+            Console.WriteLine("  uninstall                      -> remove service");
+            Console.WriteLine("  net start JarvisSampleApp      -> start service");
+            Console.WriteLine("  net stop JarvisSampleApp       -> stop service");
+            Console.WriteLine("===================================================================");
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
---
+++ src/Jarvis.ServiceHost/Support/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jarvis.Framework.Shared.IdentitySupport;
+
+namespace Jarvis.ServiceHost.Support
+{
+    public class Bootstrapper
+    {
+        public static void Init()
+        {
+            MongoFlatMapper.EnableFlatMapping(); //before any chanche that the driver scan any type.
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
